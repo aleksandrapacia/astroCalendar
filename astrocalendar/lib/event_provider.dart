566df843658251eb38367eff79f6b5e59452ce5b
ola pacia <olusiaop05@gmail.com>
2024-08-06 12:52:44 +0200

    created provider for app

--- astrocalendar/lib/event_provider.dart
+++ astrocalendar/lib/event_provider.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 
-class EventProvider with ChangeNotifier {}
+class EventProvider with ChangeNotifier {
+  final Map<DateTime, List<String>> _events = {};
+
+  Map<DateTime, List<String>> get events => _events;
+
+  void addEvent(DateTime date, String event) {
+    if (_events[date] != null) {
+      _events[date]!.add(event);
+    } else {
+      _events[date] = [event];
+    }
+    notifyListeners();
+  }
+}
--- astrocalendar/lib/main.dart
+++ astrocalendar/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'event_provider.dart';
 import 'home_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => EventProvider(),
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
